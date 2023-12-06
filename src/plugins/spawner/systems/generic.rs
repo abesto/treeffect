5cd7f1c1484544fbc7b1a6d354b086b045ca7dbc
Zoltán Nagy <abesto@abesto.net>
2023-12-06 23:42:37 +0000

    Fix energy system harder: random energy offset at spawn

--- src/plugins/spawner/systems/generic.rs
+++ src/plugins/spawner/systems/generic.rs
@@ -1,6 +1,12 @@
 use bevy::prelude::*;
+use bevy_prng::WyRand;
+use bevy_rand::resource::GlobalEntropy;
+use rand::Rng;
 
-use crate::{components::position::Position, plugins::spawner::bundles::DogBundle};
+use crate::{
+    components::{energy::Energy, position::Position},
+    plugins::spawner::bundles::DogBundle,
+};
 
 pub enum SpawnType {
     Dog,
@@ -12,10 +18,17 @@ pub struct Spawn {
     pub kind: SpawnType,
 }
 
-pub fn spawn(mut commands: Commands, mut ev: EventReader<Spawn>) {
+pub fn spawn(
+    mut commands: Commands,
+    mut ev: EventReader<Spawn>,
+    mut rng: ResMut<GlobalEntropy<WyRand>>,
+) {
     for ev in ev.read() {
         let bundle = match ev.kind {
-            SpawnType::Dog => DogBundle::default(),
+            SpawnType::Dog => DogBundle {
+                energy: Energy::new(rng.gen_range(1..=500)),
+                ..Default::default()
+            },
         };
         commands.spawn(bundle).insert(Position::from(ev.xy));
     }
